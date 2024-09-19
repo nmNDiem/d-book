3f0bd45c6611f41d0a54eaf98ecac8084b845115
nmNDdiem <diem.nmngoc@gmail.com>
2024-09-19 15:41:27 +0700

    Initial commit

---
+++ src/main/java/com/nmnd/d_book_web/DBookWebApplication.java
@@ -0,0 +1,13 @@
+package com.nmnd.d_book_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBookWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBookWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nmnd/d_book_web/DBookWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmnd.d_book_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBookWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
